f5f6b8cae306fb8b44858da14c382f67cae3a9e0
Mushkin.V <mushkin.v@gmail.com>
2014-12-20 15:14:48 +0200

    Adds index view

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,32 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\Type\ChildType;
+use AppBundle\Entity\Child;
+
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('Default/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(new ChildType(), new Child());
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $child = $form->getData();
+            $em->persist($child->getUser());
+            $em->flush();
+
+            return $this->redirect('Default/tests.html.twig');
+        }
+
+
+        return $this->render('Default/index.html.twig', array('form' => $form->createView()));
     }
 }
---
+++ src/AppBundle/Form/Type/ChildType.php
@@ -0,0 +1,34 @@
+<?php
+namespace  AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ChildType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text');
+        $builder->add('surname', 'text');
+        $builder->add('age', 'integer');
+        $builder->add('parent', 'text');
+        $builder->add('email', 'text');
+        $builder->add('letter', 'textarea');
+        $builder->add('congratulation', 'textarea');
+
+        $builder->add('Submit', 'submit');
+    }
+
+    public function getName()
+    {
+        return 'child';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Child'
+        ));
+    }
+}
