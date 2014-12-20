c2d887304b20b04d4ea7f70178bec46871a56fb6
Mushkin.V <mushkin.v@gmail.com>
2014-12-20 17:58:13 +0200

    Adds result form

--- src/AppBundle/Controller/ResultController.php
+++ src/AppBundle/Controller/ResultController.php
@@ -5,9 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use AppBundle\Form\Type\ChildType;
-use AppBundle\Entity\Child;
-
 
 class ResultController extends Controller
 {
@@ -16,8 +13,25 @@ class ResultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        
+        $points = 0;
 
-        return $this->render('Default/result.html.twig', array('form' => $form->createView()));
+        if ($request->isMethod('POST')) {
+
+            // ->setDescription($request->request->get('description'))
+            foreach ($this->getQuestions() as $question) {
+                if ($request->request->get($question->getId())) {
+                    $points=$points+$request->request->get($question->getId());
+                }
+            }
+        }
+        return $this->render('Default/result.html.twig', array('points' => $points));
+    }
+
+    public function getQuestions()
+    {
+        $questions = $this->getDoctrine()->getManager()
+            ->getRepository('AppBundle:Questions')->findAll();
+
+        return $questions;
     }
 }
